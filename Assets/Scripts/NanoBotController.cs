e8f992c18bc979280f31bc795489bff7be057f59
TastyOishi <tastykent@gmail.com>
2015-12-13 01:16:41 +1300

    Ze Graphics - I think the physics are a bit off :)

--- Assets/Scripts/NanoBotController.cs
+++ Assets/Scripts/NanoBotController.cs
@@ -44,3 +44,11 @@ public class NanoBotController : MonoBehaviour {
         }
 	}
 }
+
+public static class UnityExtensions
+{
+    public static Vector3 ScreenToWorldLength(this Camera camera, Vector3 position)
+    {
+        return camera.ScreenToWorldPoint(position) - camera.ScreenToWorldPoint(Vector3.zero);
+    }
+}
---
+++ Assets/Scripts/WallRepeater.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallRepeater : MonoBehaviour {
+
+    public  GameObject wallOne;
+    public GameObject wallTwo;
+    public GameObject floor;
+    public float wallSpeed = .20f;
+
+
+
+    public Vector3 initialPositionOne;
+    public Vector3 initialPositionTwo;
+    public double wallOneMaxLimit = 0;
+    public double wallTwoMaxLimit = 0;
+
+
+    // Use this for initialization
+    void Start () {
+    initialPositionOne = wallOne.transform.position;
+    initialPositionTwo = wallTwo.transform.position;
+        wallOneMaxLimit -= wallOne.transform.lossyScale.y ;
+        wallTwoMaxLimit -= wallTwo.transform.lossyScale.y;
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        wallOne.transform.position -= (new Vector3(0, wallSpeed));
+        wallTwo.transform.position -= (new Vector3(0, wallSpeed));
+        if (wallOne.transform.position.y <= (floor.transform.lossyScale.y * -1) -5) wallOne.transform.position = initialPositionOne;
+        if (wallTwo.transform.position.y <= (floor.transform.lossyScale.y * -1) - 5) wallTwo.transform.position = initialPositionOne;
+    }
+}
