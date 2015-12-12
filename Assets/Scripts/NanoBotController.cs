5f3573b914db100d6e00dcf019f61cb54c074621
lordkawaii@gmail.com <lordkawaii@gmail.com>
2015-12-11 23:48:47 -0800

    Changed Player controller and added nanobot controller

---
+++ Assets/Scripts/NanoBotController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NanoBotController : MonoBehaviour {
+    public float swarmSpeed;
+    public float swarmRadius;
+
+    bool hasBeenFired = false;
+    bool hasBeenPickedUp = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasBeenFired && hasBeenPickedUp)
+        {
+            try
+            {
+                //transform.position = Vector3.Lerp(transform.position,transform.parent.position + new Vector3((Random.Range(-swarmRadius, swarmRadius)), (Random.Range(-swarmRadius, swarmRadius))), Time.deltaTime * swarmSpeed);
+                transform.Translate(new Vector3(transform.parent.position.x + Random.Range(-swarmRadius, swarmRadius), transform.parent.position.y + Random.Range(-swarmRadius, swarmRadius)) * Time.deltaTime);
+            }
+            catch
+            {
+            }
+        }
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,10 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     public float movementSpeed = 1;
-    public float jumpHight = 1;
-    public float growthRate = 1;
 
     Rigidbody2D rb2D;
-    float totalMass = 0;
+    float totalNanoBots = 0;
 
-    bool isJumping = false;
     // Use this for initialization
     void Start () {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
@@ -25,30 +22,17 @@ public class PlayerController : MonoBehaviour {
         float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
 
-        if (horizontalAxis != 0)
+        if (horizontalAxis != 0 && verticalAxis == 0)
         {
             transform.Translate(new Vector3(horizontalAxis * movementSpeed, 0));
         }
-        if (verticalAxis > 0 && !isJumping)
+        if (verticalAxis != 0 && horizontalAxis ==0)
         {
-            rb2D.AddForce(new Vector2(0, jumpHight), ForceMode2D.Impulse);
-            isJumping = true;
+            transform.Translate(new Vector3(0, verticalAxis * movementSpeed));
         }
-    }
-
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Floor")
+        if (verticalAxis != 0 && horizontalAxis != 0)
         {
-            isJumping = false;
-        }
-
-        if (other.gameObject.name == "Mass")
-        {
-            Destroy(other.gameObject);
-            totalMass++;
-            transform.localScale += new Vector3(growthRate, growthRate);
+            transform.Translate(new Vector3(horizontalAxis * movementSpeed, verticalAxis * movementSpeed));
         }
     }
 
