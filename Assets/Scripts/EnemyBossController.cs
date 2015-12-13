868e034f213ba764df2696c867b687b86652181f
LordKawaii <lordkawaii@gmail.com>
2015-12-12 21:05:15 -0800

    Added boss controller, boss moves back and forth.

---
+++ Assets/Scripts/EnemyBossController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBossController : MonoBehaviour {
+    bool movingRight = true;
+
+    public float movementSpeed;
+    public float travelWidth;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movingRight && transform.position.x <= travelWidth)
+            transform.Translate(Vector2.right * Time.deltaTime * movementSpeed);
+        if (transform.position.x >= travelWidth)
+            movingRight = false;
+        if (!movingRight && transform.position.x >= -travelWidth)
+            transform.Translate(Vector2.left * Time.deltaTime * movementSpeed);
+        if (transform.position.x <= -travelWidth)
+            movingRight = true;
+    }
+}
