2a3bf9447616a8104891fa8edc9a2f8b90980fcc
LordKawaii <lordkawaii@gmail.com>
2015-12-14 00:06:44 -0800

    Added a timer script for the enemy waves.

---
+++ Assets/Scripts/EnemyWaveTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWaveTimer : MonoBehaviour {
+    public float stopingHight;
+    public float desentSpeed;
+    public float TimeTillMovingOn = 1;
+
+    Vector2 stopingPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
