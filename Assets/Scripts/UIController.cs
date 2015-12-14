2760193be8f374f6c9bfa5276cee064ab8353f34
unknown <Terry@ZENOM01W7.Zenom.local>
2015-12-14 12:41:49 -0800

    Adding UI Controller and lives images

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class UIController : MonoBehaviour
+{
+
+    public List<Image> heroLives;
+
+    PlayerController playerController;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update the number of lives images so that they match the number of player lives
+        if (playerController.lives < 3)
+           heroLives[playerController.lives].enabled = false;
+
+    }
+}
+
